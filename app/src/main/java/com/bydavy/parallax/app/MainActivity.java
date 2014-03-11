78fb3ac6b1a558e7a99db53cf990ad6a696a8d02
Davy Leggieri <bydavy@gmail.com>
2014-03-11 09:40:56 -0700

    Second wave of optimization. Layout more flat and parallax done by transfering image height to margin top

--- app/src/main/java/com/bydavy/parallax/app/MainActivity.java
+++ app/src/main/java/com/bydavy/parallax/app/MainActivity.java
@@ -3,13 +3,12 @@ package com.bydavy.parallax.app;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 
 public class MainActivity extends Activity {
 
-	private TrackingScrollView mScroller;
-	private View mImageSpacer;
 	private ImageView mImage;
 	private View mPresentation;
 	private int mImageHeight;
@@ -19,36 +18,38 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		mImageSpacer = findViewById(R.id.image_space);
-
 		mImage = (ImageView) findViewById(R.id.image);
 		mImageHeight = mImage.getLayoutParams().height;
 
-		mScroller = (TrackingScrollView) findViewById(R.id.scroller);
-		mScroller.setOnScrollChangedListener(new TrackingScrollView.OnScrollChangedListener() {
-			@Override
-			public void onScrollChanged(TrackingScrollView source, int l, int t, int oldl, int oldt) {
-				handleScroll(source, t);
-			}
-		});
+		((TrackingScrollView) findViewById(R.id.scroller)).setOnScrollChangedListener(
+				new TrackingScrollView.OnScrollChangedListener() {
+					@Override
+					public void onScrollChanged(TrackingScrollView source, int l, int t, int oldl, int oldt) {
+						handleScroll(source, t);
+					}
+				}
+		);
 
 		mPresentation = findViewById(R.id.presentation);
 		centerViewVertically(mPresentation);
 	}
 
 	private void handleScroll(TrackingScrollView source, int top) {
-		final float imageScrollingPercent = Math.min(mImageHeight, Math.max(0, top)) / (float) mImageHeight;
+		int scrolledImageHeight = Math.min(mImageHeight, Math.max(0, top));
 
-		// Increase spacer height on Y increase
-		mImageSpacer.getLayoutParams().height = (int) (mImageHeight * imageScrollingPercent);
-		mImageSpacer.setLayoutParams(mImageSpacer.getLayoutParams());
+		ViewGroup.MarginLayoutParams imageParams = (ViewGroup.MarginLayoutParams) mImage.getLayoutParams();
+		int newImageHeight = mImageHeight - scrolledImageHeight;
+		if (imageParams.height != newImageHeight) {
+			// Transfer image height to margin top
+			imageParams.height = newImageHeight;
+			imageParams.topMargin = scrolledImageHeight;
 
-		// Shrink image height on Y increase
-		mImage.getLayoutParams().height = (int) (mImageHeight * (1 - imageScrollingPercent));
-		mImage.setLayoutParams(mImage.getLayoutParams());
+			// Invalidate view
+			mImage.setLayoutParams(imageParams);
+		}
 	}
 
-	private void centerViewVertically(View view) {
+	private static void centerViewVertically(View view) {
 		view.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
 			@Override
 			public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
